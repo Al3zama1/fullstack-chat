4415339d7217cdd5b236ea6945e78535f29a93dd
Abran Lezama <lezama.abran@gmail.com>
2023-04-29 14:02:34 -0700

    Created project to start working

---
+++ src/main/java/com/abranlezama/fullstackchatapplication/FullstackChatApplication.java
@@ -0,0 +1,13 @@
+package com.abranlezama.fullstackchatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abranlezama/fullstackchatapplication/FullstackChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abranlezama.fullstackchatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
