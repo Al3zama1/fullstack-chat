dcda1320724797de25c1063b152b5dcaf197ea72
Abran Lezama <lezama.abran@gmail.com>
2023-04-30 10:34:05 -0700

    Implemented user model and repository

--- src/main/java/com/abranlezama/fullstackchatapplication/model/User.java
+++ src/main/java/com/abranlezama/fullstackchatapplication/model/User.java
@@ -8,15 +8,20 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Document
-public class User {
+public class User implements UserDetails {
     @Id
     private String id;
     @Indexed(unique = true)
@@ -24,4 +29,28 @@ public class User {
     private String password;
     private LocalDateTime lastUpdated;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/abranlezama/fullstackchatapplication/repository/UserRepository.java
+++ src/main/java/com/abranlezama/fullstackchatapplication/repository/UserRepository.java
@@ -4,5 +4,9 @@ import com.abranlezama.fullstackchatapplication.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
